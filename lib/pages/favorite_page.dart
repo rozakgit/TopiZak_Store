7b6f5dc72b64a1f316b630426dadc0be0d6d23df
Muhammad Abdul Rozak <muhammadabdulrozak21@gmail.com>
2026-04-23 17:31:40 +0700

    menambahkan halaman favorite page

---
+++ lib/pages/favorite_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/favorite_model.dart';
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final favorite = Provider.of<FavoriteModel>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Favorite")),
+
+      body: favorite.items.isEmpty
+          ? Center(child: Text("Belum ada favorit"))
+          : ListView.builder(
+        itemCount: favorite.items.length,
+        itemBuilder: (context, index) {
+          final item = favorite.items[index];
+
+          return ListTile(
+            leading: SizedBox(
+              width: 50,
+              height: 50,
+              child: Image.asset(item.image[0], fit: BoxFit.cover),
+            ),
+            title: Text(item.name),
+            subtitle: Text("Rp ${item.price}"),
+            trailing: Icon(Icons.favorite, color: Colors.red),
+          );
+        },
+      ),
+    );
+  }
+}
