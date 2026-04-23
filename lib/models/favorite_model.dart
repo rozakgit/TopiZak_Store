99aa707a5d29b6aab61c561608140c22eb91619d
Muhammad Abdul Rozak <muhammadabdulrozak21@gmail.com>
2026-04-23 17:44:34 +0700

    menambahkan model dari favorit

---
+++ lib/models/favorite_model.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'product.dart';
+
+class FavoriteModel extends ChangeNotifier {
+  final List<Product> _items = [];
+
+  List<Product> get items => _items;
+
+  void toggleFavorite(Product product) {
+    if (_items.contains(product)) {
+      _items.remove(product);
+    } else {
+      _items.add(product);
+    }
+    notifyListeners();
+  }
+
+  bool isFavorite(Product product) {
+    return _items.contains(product);
+  }
+}
